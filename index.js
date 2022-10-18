e70608081497ba376c6ca2aa575a839d4a041f42
Lucie Musilová <musil.lucie@seznam.cz>
2022-10-18 22:26:22 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('funguju!');
+import { Task } from "./Task/index.js";
+
+const check = document.querySelector("#checkbox-undone");
+
+const renderTasks = (data) => {
+  const ukoly = document.querySelector(".todo__tasks");
+  ukoly.innerHTML = data.map((item) => Task(item)).join("");
+};
+
+fetch("https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks")
+  .then((response) => {
+    return response.json();
+  })
+  .then(renderTasks);
+
+check.addEventListener("change", () => {
+  if (check.checked) {
+    fetch(
+      "https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks?done=false"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then(renderTasks);
+  } else {
+    fetch(
+      "https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then(renderTasks);
+  }
+});
